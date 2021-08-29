d9e66ec9bb7b55eee6d3a91d468f8f0c6905a4d6
Christopher Morales <Christopher.Morales@innovaresol.com>
2021-08-29 16:34:18 -0400

    Finished Success & Error Notifications module

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import * as user from '@/store/modules/user'
 import * as event from '@/store/modules/event'
+import * as notification from '@/store/modules/notification'
 
 Vue.use(Vuex)
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   modules: {
     user,
     event,
+    notification,
   },
   state: {
     categories: [
